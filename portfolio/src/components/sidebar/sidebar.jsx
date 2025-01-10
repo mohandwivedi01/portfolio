50ee064abaf211ca6f158564e02a3efa765f76ed
mohandwivedi01 <manmohandwivedi@yahoo.com>
2025-01-10 23:16:03 +0530

    add react-icons dependency and update sidebar component layout

--- portfolio/src/components/sidebar/sidebar.jsx
+++ portfolio/src/components/sidebar/sidebar.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Sidebar(){
+
     return (
-        <div className="bg-red-900">
-            
+        <div className=" text-white ">
+            <div className="flex flex-wrap py-2 bg-gray-950 ">
+                <div className="flex columns-4 w-20 justify-start">
+                    <div className="w-4 h-4 bg-yellow-500 rounded-full"></div>
+                    <div className="w-4 h-4 mx-4 bg-green-500 rounded-full"></div>
+                    <div className="w-4 h-4 bg-red-500 rounded-full"></div>              
+                </div>
+                <div className="flex flex-grow justify-center items-center">
+                    <div className="flex text-lg font-bold border rounded-md border-zinc-700 w-1/2 justify-center items-center">Mohan Dwivedi - Portfolio
+
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-row">
+                <div className="flex flex-col justify-center items-center h-screen w-16">
+                    hello1
+                </div>
+                <div className="flex flex-col justify-center items-center h-screen bg-gray-950 w-72">
+                    hello
+                </div>
+            </div>
         </div>
     )
 }
