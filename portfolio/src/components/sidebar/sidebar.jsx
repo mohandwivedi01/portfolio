5ef5860a9c4da3f77a43b7b5dd9f2968bcc1a148
mohandwivedi01 <manmohandwivedi@yahoo.com>
2025-01-13 22:49:32 +0530

    add animations

--- portfolio/src/components/sidebar/sidebar.jsx
+++ portfolio/src/components/sidebar/sidebar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { FaWhatsapp, FaChevronDown, FaLinkedin, FaTwitter, FaGithub, FaEnvelope, FaReact, FaHtml5, FaJs, FaPython, FaHashtag, FaExclamationTriangle,  FaCheckDouble, FaCodeBranch, FaRegTimesCircle, FaBezierCurve, FaDiscord, FaClone, FaJava, FaUser, FaRegUserCircle, FaRegSun } from "react-icons/fa";
-import { NavLink } from "react-router";
+import { FaWhatsapp, FaChevronDown, FaLinkedin, FaTwitter, FaGithub, FaEnvelope, FaReact, FaHtml5, FaJs, FaPython, FaHashtag, FaExclamationTriangle,  FaCheckDouble, FaCodeBranch, FaRegTimesCircle, FaBezierCurve, FaDiscord, FaClone, FaJava, FaUser, FaRegUserCircle, FaRegSun, FaCss3, FaCut, FaBackspace } from "react-icons/fa";
+import { NavLink, Link, Links } from "react-router";
 
 function Sidebar(){
-    const isActive = false;
+
+    const github = "https://github.com/mohandwivedi01/";
+    const linkedin = "https://www.linkedin.com/in/mohan-dwivedi/";
+    const twitter = "https://x.com/ByteByByte_";
+    const whatsapp = "https://wa.me/7518164359";
+    const email = "mailto:mohandwivedi1806@gmail.com?subject=Inquiry&message=Hi,%20Mohan";
+
     return (
         <div className="bg-neutral-800 text-white h-screen w-full flex flex-col ">
             {/* header started */}
-            <nav className="bg-gray-950">
+            <NavLink className="bg-gray-950">
                 <div className="flex flex-wrap py-2">
                     <div className="flex columns-4 w-20 ml-2 justify-start">
                         <div className="w-4 h-4 bg-yellow-500 rounded-full"></div>
@@ -15,76 +21,96 @@ function Sidebar(){
                         <div className="w-4 h-4 bg-red-500 rounded-full"></div>              
                     </div>
                     <div className="flex flex-grow justify-center items-center">
-                        <div className="flex text-lg font-bold border rounded-md border-zinc-700 w-1/2 justify-center items-center bg-zinc-800">Mohan Dwivedi - Portfolio
+                        <div className="flex text-lg font-bold border rounded-md border-zinc-700 w-1/2 justify-center items-center bg-zinc-800 hover:text-orange-500">Mohan Dwivedi - Portfolio
                         </div>
                     </div>
                 </div>
-            </nav>
+            </NavLink>
             {/* header end */}
 
             {/* main section started */}
             <div className="flex-1 grid grid-cols-[65px_1fr_4fr] ">
-                {/* links */}
+
+                {/* side links */}
                 <div className="flex flex-col justify-between items-center w-16 ">
                     <div className="flex flex-col justify-around items-center h-1/2">
-                        <FaClone size={30} color="gray" />
-                        <FaGithub size={30} color="gray" />
-                        <FaLinkedin size={30} color="gray" />
-                        <FaTwitter size={30} color="gray" />
-                        <FaWhatsapp size={30} color="gray" />
-                        <FaEnvelope  size={30} color="gray" />
+                        <Link to=""><FaClone size={30} color="gray" /></Link>
+                        <a href={github} target="_blank" ><FaGithub size={30} color="gray"/></a>
+                        <a href={linkedin} target="_blank" ><FaLinkedin size={30} color="gray" /></a>
+                        <a href={twitter} target="_blank" ><FaTwitter size={30} color="gray" /></a>
+                        <a href={whatsapp} target="_blank" ><FaWhatsapp size={30} color="gray" /></a>
+                        <a href={email} target="_blank" ><FaEnvelope  size={30} color="gray" /></a>
                     </div>
                     <div className="flex flex-col justify-around items-center h-1/6 mb-5">
-                        <FaRegUserCircle size={30} color="gray" />
-                        <FaRegSun size={30} color="gray" />
+                        <Link to="/"><FaRegUserCircle size={30} color="gray" /></Link>
+                        <Link to="/"><FaRegSun size={30} color="gray" /></Link>
                     </div>
                 </div>
-                {/* explorer */}
-                <div className="bg-gray-950 px-4">
+                {/* side links end */}
+
+                {/* side explorer */}
+                <div className="bg-gray-950 pl-4">
                     <div className="flex flex-col">
                         <label className="label text-md text-gray-500 font-bold">EXPLORER</label>
                     </div>
+                    
                     <div className=" flex flex-wrap flex-col my-4">
                         <div className="flex flex-row my-1">
                             <label className="label text-md font-bold"><FaChevronDown size={20} /></label>
                             <label className="label text-md font-bold px-1">PORTFOLIO</label>
                         </div>
                         <div className="flex flex-col my-2 ml-3">
-                            <div className="flex flex-row my-1">
-                                <label><FaReact size={22} color="#39cfe3" /></label>
-                                <label className="label text-md  px-1">home.jsx</label>
-                            </div>
-                            <div className="flex flex-row my-1">
-                                <label><FaHashtag size={22} color="#08eab6" /></label>
-                                <label className="label text-md  px-1">about.css</label>
-                            </div>
-                            <div className="flex flex-row my-1">
-                                <label><FaHtml5 size={22} color="#ff663d" /></label>
-                                <label className="label text-md  px-1">resume.html</label>
-                            </div>
-                            <div className="flex flex-row my-1">
-                                <label><FaJs size={22} color="#f0db4f" /></label>
-                                <label className="label text-md  px-1">contact.js</label>
-                            </div>
-                            <div className="flex flex-row my-1">
-                                <label><FaJava size={22} color="#f8553c" /></label>
-                                <label className="label text-md  px-1">github.java</label>
-                            </div>
-                            <div className="flex flex-row my-1">
-                                <label><FaPython size={22} color="#66ff15" /></label>
-                                <label className="label text-md  px-1">blog.py</label>
-                            </div>
+                            <ul className="flex flex-col">
+                                <li>
+                                    <NavLink to="/" className={({isActive}) => `py-2 flex px-4 items-center gap-2 duration-200 ${isActive ? "bg-neutral-800 border-l border-l-orange-700 border-t border-t-orange-700 rounded-l-3xl" : "border border-gray-950"}    hover:text-orange-500`}>
+                                        <FaReact size={22} color="#39cfe3" />
+                                        <span className="">home.jsx</span>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/about" className={({isActive}) => `py-2 flex px-4 items-center gap-2 duration-200 ${isActive ? "bg-neutral-800 border-l border-l-orange-700 border-t border-t-orange-700 rounded-l-3xl" : "border border-gray-950"}    hover:text-orange-500`}>
+                                        <FaHtml5 size={22} color="#ff663d" />
+                                        <span className="">about.html</span>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/resume" className={({isActive}) => `py-2 flex px-4 items-center gap-2 duration-200 ${isActive ? "bg-neutral-800 border-l border-l-orange-700 border-t border-t-orange-700 rounded-l-3xl" : "border border-gray-950"}    hover:text-orange-500`}>
+                                        <FaHashtag size={22} color="#08eab6" />
+                                        <span className="">resume.css</span>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/contact" className={({isActive}) => `py-2 flex px-4 items-center gap-2 duration-200 ${isActive ? "bg-neutral-800 border-l border-l-orange-700 border-t border-t-orange-700 rounded-l-3xl" : "border border-gray-950"}    hover:text-orange-500`}>
+                                        <FaJs size={22} color="#f6ec3f" />
+                                        <span className="">contact.js</span>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/github" className={({isActive}) => `py-2 flex px-4 items-center gap-2 duration-200 ${isActive ? "bg-neutral-800 border-l border-l-orange-700 border-t border-t-orange-700 rounded-l-3xl" : "border border-gray-950"}    hover:text-orange-500`}>
+                                        <FaJava size={22} color="#f8553c" />
+                                        <span className="">github.java</span>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/blog" className={({isActive}) => `py-2 flex px-4 items-center gap-2 duration-200 ${isActive ? "bg-neutral-800 border-l border-l-orange-700 border-t border-t-orange-700 rounded-l-3xl" : "border border-gray-950"}    hover:text-orange-500`}>
+                                        <FaPython size={22} color="#66ff15" />
+                                        <span className="">blog.py</span>
+                                    </NavLink>
+                                </li>
+                                
+                            </ul>                    
                         </div>
                     </div>
                 </div>
-                {/* editor */}
+
+                {/* open pages */}
                 <div className="">
                     <div className="bg-gray-950 flex flex-row border-t border-neutral-800">
                         <ul className="flex flex-col lg:flex-row  lg:mt-0">
                             <li className="">
                             <NavLink 
                                     to="/"
-                                    className={({isActive}) => `px-5 py-[6px] flex items-center gap-2 duration-200 ${isActive ? "bg-neutral-800 border-t border-t-orange-700 border border-neutral-800": "border border-neutral-800"}    hover:text-orange-600`}>
+                                    className={({isActive}) => `px-5 py-[6px] flex items-center gap-2 duration-200 ${isActive ? "bg-neutral-800 border-t border-t-orange-700 border border-neutral-800": "border border-neutral-800"}    hover:text-orange-500`}>
                                     <FaReact size={22} color="#39cfe3" />
                                     <span className="">home.jsx</span>
                                 </NavLink>
@@ -92,15 +118,17 @@ function Sidebar(){
                             <li className="">
                             <NavLink 
                                     to="/about"
-                                    className={({isActive}) => ` px-5 py-[6px] flex items-center gap-2 duration-200 ${isActive ? "bg-neutral-800 border-t border-t-orange-700 border border-neutral-800": "border border-neutral-800"}    hover:text-orange-600`}>
+                                    className={({isActive}) => ` pl-5 pr-2 py-[6px] flex items-center gap-2 duration-200 ${isActive ? "block bg-neutral-800 border-t border-t-orange-700 border border-neutral-800": "hidden border border-neutral-800"}    hover:text-orange-500`}>
                                     <FaHtml5 size={22} color="#ff663d" />
                                     <span className="">about.html</span>
+                                    {/* we'll look after some time */}
+                                    <FaBackspace size={20} className={() => `text-neutral-800 hover:text-neutral-500 `}/>
                                 </NavLink>
                             </li>
                             <li className="">
                             <NavLink 
                                     to="/resume"
-                                    className={({isActive}) => ` px-5 py-[6px] flex items-center gap-2 duration-200 ${isActive ? "bg-neutral-800 border-t border-t-orange-700 border border-neutral-800": "border border-neutral-800"}    hover:text-orange-600`}>
+                                    className={({isActive}) => ` px-5 py-[6px] flex items-center gap-2 duration-200 ${isActive ? "block bg-neutral-800 border-t border-t-orange-700 border border-neutral-800": "hidden border border-neutral-800"}    hover:text-orange-500`}>
                                     <FaHashtag size={22} color="#08eab6" />
                                     <span className="">resume.css</span>
                                 </NavLink>
@@ -108,7 +136,7 @@ function Sidebar(){
                             <li className="">
                             <NavLink 
                                     to="/contact"
-                                    className={({isActive}) => ` px-5 py-[6px] flex items-center gap-2 duration-200 ${isActive ? "bg-neutral-800 border-t border-t-orange-700 border border-neutral-800": "border border-neutral-800"}    hover:text-orange-600`}>
+                                    className={({isActive}) => ` px-5 py-[6px] flex items-center gap-2 duration-200 ${isActive ? "block bg-neutral-800 border-t border-t-orange-700 border border-neutral-800": "hidden border border-neutral-800"}    hover:text-orange-500`}>
                                     <FaJs size={22} color="#f6ec3f" />
                                     <span className="">conatct.js</span>
                                 </NavLink>
@@ -116,7 +144,7 @@ function Sidebar(){
                             <li className="">
                             <NavLink 
                                     to="/github"
-                                    className={({isActive}) => ` px-5 py-[6px] flex items-center gap-2 duration-200 ${isActive ? "bg-neutral-800 border-t border-t-orange-700 border border-neutral-800": "border border-neutral-800"}    hover:text-orange-600`}>
+                                    className={({isActive}) => ` px-5 py-[6px] flex items-center gap-2 duration-200 ${isActive ? "block bg-neutral-800 border-t border-t-orange-700 border border-neutral-800": "hidden border border-neutral-800"}    hover:text-orange-500`}>
                                     <FaJava size={22} color="#f8553c" />
                                     <span className="">github.java</span>
                                 </NavLink>
@@ -124,7 +152,7 @@ function Sidebar(){
                             <li className="">
                             <NavLink 
                                     to="/blog"
-                                    className={({isActive}) => ` px-5 py-[6px] flex items-center gap-2 duration-200 ${isActive ? "bg-neutral-800 border-t border-t-orange-700 border border-neutral-800": "border border-neutral-800"}    hover:text-orange-600`}>
+                                    className={({isActive}) => ` px-5 py-[6px] flex items-center gap-2 duration-200 ${isActive ? "block bg-neutral-800 border-t border-t-orange-700 border border-neutral-800": "hidden border border-neutral-800"}    hover:text-orange-500`}>
                                     <FaPython size={22} color="#66ff15" />
                                     <span className="">blog.py</span>
                                 </NavLink>
